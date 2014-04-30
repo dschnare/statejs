28698d37b8824d88f1f601f1c7a7b2ba6782ca8d
Darren Schnare <schnare.d@gmail.com>
2014-04-29 22:01:54 -0400

    Refactor the code base to use constructor-kit and property-kit, refactor game.js

---
+++ state.js
@@ -0,0 +1,212 @@
+/**
+ * @preserve Module: statejs
+ * Author: Darren Schnare
+ * Keywords: javascript,state,machine,finite,states
+ * License: MIT ( http://www.opensource.org/licenses/mit-license.php )
+ * Repo: https://github.com/dschnare/statejs
+ */
+
+/*globals 'setTimeout', 'requestAnimationFrame' */
+(function (global, constructorKit, propertyKit) {
+  'use strict';
+
+  function module(constructorKit, propertyKit) {
+    var State, Transition, isArray, invoke, async;
+
+    isArray = Array.isArray || function (o) {
+      return ({}).toString.call(o) === '[object Array]';
+    };
+
+    // invoke(o, methodName, ...)    
+    // invoke(fn, thisObj, ...)
+    invoke = function (o, methodName) {
+      var args = [].slice.call(arguments, 2);
+      
+      if (typeof o === 'function') {
+        return args.length ? o.apply(methodName, args) : o.call(methodName);
+      } else if (o && typeof o[methodName] === 'function') {
+        return args.length ? o[methodName].apply(o, args) : o[methodName]();
+      }
+    };
+
+    // async(fn)
+    // async(fn, ...)
+    async = (function () {
+      var callLater = typeof requestAnimationFrame === 'function' ? requestAnimationFrame : function (fn) { setTimeout(fn, 10); };
+      return function (fn) {
+        var args = [].slice.call(arguments, 1);
+        callLater(args.length === 0 ? fn : function () {
+          fn.apply(undefined, args);
+        });
+      };
+    }());
+    
+    State = (function () {
+      return constructorKit(function (state) {
+        this.state = propertyKit({value: state });
+      }, {
+        name: function () {
+          return this.state() ? this.state().name : '';
+        },
+        onentry: function () {
+          async(invoke, this.state(), 'onentry');
+        },
+        onexit: function () {
+          async(invoke, this.state(), 'onexit');
+        }
+      });
+    }());
+
+    Transition = (function () {
+      return constructorKit(function (transition) {
+       this.transition = propertyKit({value: transition }); 
+      }, {
+        from: function () {
+          return this.transition() ? this.transition().from : '';
+        },
+        to: function () {
+          return this.transition() ? this.transition().to : '';
+        },
+        actions: function () {
+          var transition = this.transition();
+          if (transition) {
+            if (isArray(transition.actions)) {
+              return transition.actions;
+            }
+            if (typeof transition.action === 'function') {
+              return [transition.action];
+            }
+          }
+          return [];
+        },
+        triggers: function () {
+          var transition, triggers;
+          
+          triggers = []; 
+          transition = this.transition();
+
+          if (transition) {
+            if (isArray(transition.triggers)) {
+              triggers = transition.triggers;
+            } else if (transition.trigger) {
+              triggers = [transition.trigger];
+            }
+          }
+
+          return triggers;
+        },
+        guard: function (event) {
+          var transition = this.transition();
+          return !!invoke(transition, 'guard', event); 
+        },
+        invokeActions: function (event) {
+          var actions, action, i, len, transition;
+
+          actions = this.actions();
+          transtion = this.transition();
+          len = actions.length;
+
+          for (i = 0; i < len; i += 1) {
+            action = actions[i];
+
+            if (typeof action === 'function') {
+              async(invoke, action, transition, event);
+            }
+          }
+        }
+      });
+    }());
+
+    StateMachine = (function () {
+      var key = {};
+      return constructorKit(function (initialState) {
+        this.states = propertyKit({value: {}, key: key});
+        this.transitions = propertyKit({value: [], key: key});
+        this.initialState = propertyKit({keys: {set: key}});
+        this.state = propertyKit({
+          set: function (currentValue, newValue) {
+            var states = this.states({key: key});
+
+            if (newValue === null || newValue === undefined) {
+              throw new Error('Invalid state:' + value);
+            }
+
+            newValue = newValue.toString();
+
+            if (!states[newValue]) {
+              throw new Error('State does not exist:' + newValue);
+            }
+
+            if (states[currentValue]) states[currentValue].onexit();
+            states[newValue].onentry();
+
+            return newValue;
+          }
+        });
+
+        initialState = this.addState(initialState || 'start');
+        this.initialState(initialState.name, {key: key});
+        this.state(initialState.name());
+      }, {
+        // addState(name)
+        // addState({name[, onentry, onexit]})
+        addState: function (state) {
+          if (!state || typeof state.name !== 'string') {
+            throw new Error('State must have a name property.');
+          }
+
+          if (typeof state === 'string') {
+            state = {name: state};
+          }
+
+          return this.states({key: key})[state.name] = new State(state);
+        },
+        // addTransition({from, to, [guard, [action or actions], [trigger or triggers]]})
+        addTransition: function (transition) {
+          if (!transition || typeof transition.from !== 'function' || typeof transition.to !== 'function') {
+            throw new Error('Transition must at least have a "from" and "to" poperties.");
+          }
+
+          this.transitions({key: key}).push(new Transition(transition));
+        },
+        trigger: function (event) {
+          var states, transitions, eventType, i, len, state, transition;
+
+          if (!event || (typeof event  !== 'string' && !event.type)) {
+            throw new Error('Event must either be a string or an object with a type property.');
+          }
+
+          eventType = event.type || event;
+          states = this.states({key: key});
+          state = states[this.state()];
+          transitions = this.transitions({key: key});
+
+          len = transitions.length;
+          for (i = 0; i < len; i += 1) {
+            transition = transitions[i];
+
+            if (transition.from() === state.name() && transition.hasTrigger(eventType) && transition.guard(event)) {
+              toState = states[transition.to()];
+
+              if (toState) {
+                transition.invokeActions(event);
+                this.state(toState.name());
+                break;
+              }
+            }
+          }
+        }
+      });
+    }());
+
+    return StateMachine;
+  }
+
+  if (typeof exports === 'object' && exports) {
+    exports.StateMachine = module(require('constructor-kit'), require('property-kit'));
+  } else if (typeof define === 'function' && define.amd) {
+    define(['constructor-kit', 'property-kit'], module);
+  else {
+    global.StateMachine = module(constructorKit, propertyKit);
+  }
+}(this, this.constructorKit, this.propertyKit));
--- test/js/game.js
+++ test/js/game.js
@@ -32,250 +32,250 @@
 // - (condition) is the guard condition used to determine if transition should be invoked.
 
 $(function () {
-	var sm = STATEJS.makeStateMachine(),
-			// The current level (1-based).
-		level = 0,
-			// The player's total score.
-		score = 0,
-			// The number of levels.
-		levelCount = 5,
-			// The goal (i.e. number of lines to clear) for each level.
-		goal = [3, 3, 3, 3, 3],
-			// The lines cleared for the current level.
-		linesCleared = 0,
-			// The options the player has set for the game.
-		options = {},
-			// The asset loader.
-		loader = makeLoader(),
-			// The input manager.
-		inputManager = makeInputManager(document),
-			// The screen manager.
-		screenManager = makeScreenManager(document.body, inputManager);
+  var sm, level, score, levelCount, goal, linesCleared, options, loader, inputManager, screenManager;
 
-	// Our screen manager takes care of displaying and rendering
-	// our game screens. We set the clear colour (i.e. background) to
-	// solid black and tell it to fade between screens.
-	screenManager.clearColour('#000000');
-	screenManager.transition('fade');
+  sm = new StateMachine();
+  // The current level (1-based).
+  level = 0;
+  // The player's total score.
+  score = 0;
+  // The number of levels.
+  levelCount = 5;
+  // The goal (i.e. number of lines to clear) for each level.
+  goal = [3, 3, 3, 3, 3];
+  // The lines cleared for the current level.
+  linesCleared = 0;
+  // The options the player has set for the game.
+  options = {};
+  // The asset loader.
+  loader = makeLoader();
+  // The input manager.
+  inputManager = makeInputManager(document);
+  // The screen manager.
+  screenManager = makeScreenManager(document.body, inputManager);
 
-	// Our input manager takes care of interfacing with the keyboard
-	// and mouse. Here we setup a hypothetical key mapping. For this
-	// game the input manager will raise events with the event type
-	// equalling the key pressed or the key's mapped value.
-	inputManager.keyMapping({
-		ESC: 'PAUSE',
-		P: 'PAUSE',
-		LEFT: 'LEFT',
-		A: 'LEFT',
-		RIGHT: 'RIGHT',
-		D: 'RIGHT',
-		DOWN: 'DROP',
-		S: 'DROP',
-		SPACE: 'ADVANCE'
-	});
+  // Our screen manager takes care of displaying and rendering
+  // our game screens. We set the clear colour (i.e. background) to
+  // solid black and tell it to fade between screens.
+  screenManager.clearColour('#000000');
+  screenManager.transition('fade');
 
-	// Cause our actions to be invoked asynchronously
-	// so we don't block the main thread.
-	sm.invokeActionsAsynchronously(true);
+  // Our input manager takes care of interfacing with the keyboard
+  // and mouse. Here we setup a hypothetical key mapping. For this
+  // game the input manager will raise events with the event type
+  // equalling the key pressed or the key's mapped value.
+  inputManager.keyMapping({
+    ESC: 'PAUSE',
+    P: 'PAUSE',
+    LEFT: 'LEFT',
+    A: 'LEFT',
+    RIGHT: 'RIGHT',
+    D: 'RIGHT',
+    DOWN: 'DROP',
+    S: 'DROP',
+    SPACE: 'ADVANCE'
+  });
 
+  // All actions from the state machine are invoked asynchronously
+  // so they don't block the main thread.
 
-	////////////
-	// States //
-	////////////
+  ////////////
+  // States //
+  ////////////
 
-	sm.addState({
-		name: 'splash',
-		onentry: function () {
-			screenManager.show('splash');
-		}
-	});
+  sm.addState({
+    name: 'splash',
+    onentry: function () {
+      screenManager.show('splash';
+    }
+  });
 
-	sm.addState({
-		name: 'title',
-		onentry: function () {
-			level = 0;
-			score = 0;
-			screenManager.show('title');
-		}
-	});
+  sm.addState({
+    name: 'title',
+    onentry: function () {
+      level = 0;
+      score = 0;
+      screenManager.show('title');
+    }
+  });
 
-	sm.addState({
-		name: 'options',
-		onentry: function () {
-			screenManager.show('options', options);
-		}
-	});
+  sm.addState({
+    name: 'options',
+    onentry: function () {
+      screenManager.show('options', options);
+    }
+  });
 
-	sm.addState({
-		name: 'loading',
-		onentry: function () {
-			level += 1;
-			linesCleared = 0;
-			screenManager.show('loading', {level: level});
-			loader.load(level);
-		}
-	});
+  sm.addState({
+    name: 'loading',
+    onentry: function () {
+      level += 1;
+      linesCleared = 0;
+      screenManager.show('loading', {level: level});
+      loader.load(level);
+    }
+  });
 
-	sm.addState({
-		name: 'game',
-		onentry: function () {
-			screenManager.show('game', {score: score});
-		}
-	});
+  sm.addState({
+    name: 'game',
+    onentry: function () {
+      screenManager.show('game', {score: score});
+    }
+  });
 
-	sm.addState({
-		name: 'pause',
-		onentry: function () {
-			screenManager.showModal('pause');
-		},
-		onexit: function () {
-			screenManager.hideModal();
-		}
-	});
+  sm.addState({
+    name: 'pause',
+    onentry: function () {
+      screenManager.showModal('pause');
+    },
+    onexit: function () {
+      screenManager.hideModal();
+    }
+  });
 
-	sm.addState({
-		name: 'level complete',
-		onentry: function () {
-			screenManager.show('level_complete', {level: level, score: score});
-		}
-	});
+  sm.addState({
+    name: 'level complete',
+    onentry: function () {
+      screenManager.show('level_complete', {level: level, score: score});
+    }
+  });
 
-	sm.addState({
-		name: 'game complete',
-		onentry: function () {
-			screenManager.show('game_complete', {level: level, score: score});
-		}
-	});
+  sm.addState({
+    name: 'game complete',
+    onentry: function () {
+      screenManager.show('game_complete', {level: level, score: score});
+    }
+  });
 
-	sm.addState({
-		name: 'credits',
-		onentry: function () {
-			screenManager.show('credits');
-		}
-	});
-
-
-	/////////////////
-	// Transitions //
-	/////////////////
-
-	sm.addTransition({from: 'splash', to: 'title', trigger: 'SPLASH_COMPLETE'});
-	sm.addTransition({from: 'title', to: 'options', trigger: 'CHOICE_OPTIONS'});
-	sm.addTransition({
-		from: 'options',
-		to: 'title',
-		trigger: 'OPTIONS_SAVE',
-		action: function (event) {
-			// This is where we would save our options.
-			options = event.options;
-		}
-	});
-	sm.addTransition({from: 'title', to: 'loading', trigger: 'CHOICE_PLAY_GAME'});
-	sm.addTransition({from: 'loading', to: 'game', trigger: 'LOAD_COMPLETE'});
-	sm.addTransition({from: 'game', to: 'pause', trigger: 'GAME_PAUSE'});
-	sm.addTransition({from: 'pause', to: 'title', trigger: 'CHOICE_QUIT'});
-	sm.addTransition({from: 'pause', to: 'game', trigger: 'GAME_PAUSE'});
-
-	sm.addTransition({
-		from: 'game',
-		to: 'level complete',
-		trigger: 'GAME_CLEAR_LINE',
-		guard: function () {
-			return linesCleared === goal[level - 1];
-		}
-	});
-
-	sm.addTransition({
-		from: 'level complete',
-		to: 'loading',
-		trigger: 'ADVANCE',
-		guard: function () {
-			return level < levelCount;
-		}
-	});
-
-	sm.addTransition({
-		from: 'level complete',
-		to: 'game complete',
-		trigger: 'ADVANCE',
-		guard: function () {
-			return level === levelCount;
-		}
-	});
-
-	sm.addTransition({from: 'game complete', to: 'credits', trigger: 'ADVANCE'});
-	sm.addTransition({from: 'credits', to: 'title', trigger: 'ADVANCE'});
+  sm.addState({
+    name: 'credits',
+    onentry: function () {
+      screenManager.show('credits');
+    }
+  });
 
 
-	/////////////////////
-	// Event Listeners //
-	/////////////////////
+  /////////////////
+  // Transitions //
+  /////////////////
 
-	screenManager.addEventListener('SPLASH_COMPLETE', function () {
-		sm.trigger('SPLASH_COMPLETE');
-	});
+  sm.addTransition({from: 'splash', to: 'title', trigger: 'SPLASH_COMPLETE'});
+  sm.addTransition({from: 'title', to: 'options', trigger: 'CHOICE_OPTIONS'});
+  sm.addTransition({
+    from: 'options',
+    to: 'title',
+    trigger: 'OPTIONS_SAVE',
+    action: function (event) {
+      // This is where we would save our options.
+      options = event.options;
+    }
+  });
+  sm.addTransition({from: 'title', to: 'loading', trigger: 'CHOICE_PLAY_GAME'});
+  sm.addTransition({from: 'loading', to: 'game', trigger: 'LOAD_COMPLETE'});
+  sm.addTransition({from: 'game', to: 'pause', trigger: 'GAME_PAUSE'});
+  sm.addTransition({from: 'pause', to: 'title', trigger: 'CHOICE_QUIT'});
+  sm.addTransition({from: 'pause', to: 'game', trigger: 'GAME_PAUSE'});
 
-	screenManager.addEventListener('CHOICE', function (event) {
-		switch (event.choice) {
-		case 'OPTIONS':
-			sm.trigger('CHOICE_OPTIONS');
-			break;
-		case 'PLAY_GAME':
-			sm.trigger('CHOICE_PLAY_GAME');
-			break;
-		case 'QUIT':
-			sm.trigger('CHOICE_QUIT');
-			break;
-		case 'RESUME':
-			sm.trigger('GAME_PAUSE');
-			break;
-		}
-	});
+  sm.addTransition({
+    from: 'game',
+    to: 'level complete',
+    trigger: 'GAME_CLEAR_LINE',
+    guard: function () {
+      return linesCleared === goal[level - 1];
+    }
+  });
 
-	screenManager.addEventListener('OPTIONS_SAVE', function (event) {
-		sm.trigger({type: 'OPTIONS_SAVE', options: event.options});
-	});
+  sm.addTransition({
+    from: 'level complete',
+    to: 'loading',
+    trigger: 'ADVANCE',
+    guard: function () {
+      return level < levelCount;
+    }
+  });
 
+  sm.addTransition({
+    from: 'level complete',
+    to: 'game complete',
+    trigger: 'ADVANCE',
+    guard: function () {
+      return level === levelCount;
+    }
+  });
 
-	// Listen to the PROGRESS event from the loader and send a message
-	// containing the load progress to the active screens in the screen manager.
-	loader.addEventListener('PROGRESS', function (event) {
-		screenManager.send('LOAD_PROGRESS', {progress: event.progress});
-	});
-
-	loader.addEventListener('COMPLETE', function () {
-		sm.trigger('LOAD_COMPLETE');
-	});
-
-
-	inputManager.addEventListener('PAUSE', function () {
-		sm.trigger('GAME_PAUSE');
-	});
-
-	// We allow players to advance/skip a screen by pressing the
-	// 'ADVANCE' button on the keyboard (i.e. the spacebar).
-	inputManager.addEventListener('ADVANCE', function () {
-		switch (sm.state()) {
-		case 'level complete':
-		case 'game complete':
-		case 'credits':
-			sm.trigger('ADVANCE');
-			break;
-		}
-	});
+  sm.addTransition({from: 'game complete', to: 'credits', trigger: 'ADVANCE'});
+  sm.addTransition({from: 'credits', to: 'title', trigger: 'ADVANCE'});
 
 
-	screenManager.addEventListener('CLEAR_LINE', function (event) {
-		linesCleared += 1;
-		score = event.score;
-		sm.trigger('GAME_CLEAR_LINE');
-	});
+  /////////////////////
+  // Event Listeners //
+  /////////////////////
 
-	///////////////////
-	// Initial State //
-	///////////////////
+  screenManager.addEventListener('SPLASH_COMPLETE', function () {
+    sm.trigger('SPLASH_COMPLETE');
+  });
 
-	// Start the whole sequence by setting the state of the
-	// state machine to be splash.
-	sm.state('splash');
+  screenManager.addEventListener('CHOICE', function (event) {
+    switch (event.choice) {
+    case 'OPTIONS':
+      sm.trigger('CHOICE_OPTIONS');
+      break;
+    case 'PLAY_GAME':
+      sm.trigger('CHOICE_PLAY_GAME');
+      break;
+    case 'QUIT':
+      sm.trigger('CHOICE_QUIT');
+      break;
+    case 'RESUME':
+      sm.trigger('GAME_PAUSE');
+      break;
+    }
+  });
+
+  screenManager.addEventListener('OPTIONS_SAVE', function (event) {
+    sm.trigger({type: 'OPTIONS_SAVE', options: event.options});
+  });
+
+
+  // Listen to the PROGRESS event from the loader and send a message
+  // containing the load progress to the active screens in the screen manager.
+  loader.addEventListener('PROGRESS', function (event) {
+    screenManager.send('LOAD_PROGRESS', {progress: event.progress});
+  });
+
+  loader.addEventListener('COMPLETE', function () {
+    sm.trigger('LOAD_COMPLETE');
+  });
+
+
+  inputManager.addEventListener('PAUSE', function () {
+    sm.trigger('GAME_PAUSE');
+  });
+
+  // We allow players to advance/skip a screen by pressing the
+  // 'ADVANCE' button on the keyboard (i.e. the spacebar).
+  inputManager.addEventListener('ADVANCE', function () {
+    switch (sm.state()) {
+    case 'level complete':
+    case 'game complete':
+    case 'credits':
+      sm.trigger('ADVANCE');
+      break;
+    }
+  });
+
+
+  screenManager.addEventListener('CLEAR_LINE', function (event) {
+    linesCleared += 1;
+    score = event.score;
+    sm.trigger('GAME_CLEAR_LINE');
+  });
+
+  ///////////////////
+  // Initial State //
+  ///////////////////
+
+  // Start the whole sequence by setting the state of the
+  // state machine to be splash.
+  sm.state('splash');
 });
